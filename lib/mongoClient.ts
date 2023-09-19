5127ed004197b58fb7487111fd8a45046ff2be55
Byte-walker <mdshahidulridoy@gmail.com>
2023-09-19 10:25:07 +0600

    mongouri error fixed

--- lib/mongoClient.ts
+++ lib/mongoClient.ts
@@ -1,5 +1,5 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-const uri = process.env.mongoUri;
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
