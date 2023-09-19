ef199ba8b0aeab76bb706fc620c40778f97a762c
Byte-walker <mdshahidulridoy@gmail.com>
2023-09-19 09:24:24 +0600

    typescript issue fixed in authorize function

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -22,34 +22,53 @@ const authOptions = {
       // You can specify whatever fields you are expecting to be submitted.
       // e.g. domain, username, password, 2FA token, etc.
       credentials: {
-        username: {
+        email: {
           label: "Email",
           type: "email",
           placeholder: "Ex. mdshahidulridoy@gmail.com",
         },
         password: { label: "Password", type: "password" },
       },
-      async authorize<Record>(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = credentials;
 
-        if (user) {
+      // async authorize(credentials, req) {
+      //   // Add logic here to look up the user from the credentials supplied
+      //   const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+      //   if (user) {
+      //     // Any object returned will be saved in `user` property of the JWT
+      //     return user
+      //   } else {
+      //     // If you return null then an error will be displayed advising the user to check their details.
+      //     return null
+
+      //     // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+      //   }
+      // }
+
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+
+        if (credentials?.email && credentials?.password) {
           // Any object returned will be saved in `user` property of the JWT
           try {
             const mongo = await clientPromise;
             const db = mongo.db("TodoWin");
             const usersCursor = db.collection("users");
-            const found = await usersCursor.findOne({ email: user?.email });
+            const found = await usersCursor.findOne({
+              email: credentials?.email,
+            });
             if (found) {
-              const { password } = user;
-              const matched = await bcrypt.compare(password, found.password);
-              return matched
-                ? {
-                    ...user,
-                    name: found.firstName + " " + found.lastName,
-                  }
-                : null;
+              const { password } = credentials;
+              const isMatched = await bcrypt.compare(password, found.password);
+              if (isMatched) {
+                return {
+                  ...credentials,
+                  name: found.firstName + " " + found.lastName,
+                  id: "1",
+                };
+              }
             }
+            return null;
           } catch (error) {
             throw new Error("Failed to connect the database!");
           }
